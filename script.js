d23fcce9615e9b818ba52244cc5c005bd9a5ec51
Ayan Ansari <mayan9917@gmail.com>
2024-07-24 23:14:27 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const filterButtons = document.querySelector(".categories ul");
+const menuIcon = document.querySelector(".logo-section i");
+const sidebar = document.querySelector("aside");
+const container = document.querySelector(".container");
+const hideItems = document.querySelectorAll(".show");
+const cardContainer = document.querySelectorAll(".card-container a");
+
+const filterCards = (e) => {
+  document.querySelector(".all-active").classList.remove("all-active");
+  if (e.target != filterButtons) {
+    e.target.classList.add("all-active");
+  }
+  cardContainer.forEach((card) => {
+    card.classList.add("show", "hide");
+    if (
+      e.target.dataset.name === card.dataset.name ||
+      e.target.dataset.name === "all"
+    ) {
+      card.classList.remove("show", "hide");
+    }
+  });
+};
+
+menuIcon.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+  container.classList.toggle("active");
+  hideItems.forEach((item) => {
+    item.classList.toggle("hide");
+  });
+});
+filterButtons.addEventListener("click", filterCards);
